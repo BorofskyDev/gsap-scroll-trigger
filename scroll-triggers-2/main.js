1b35ccfe50b36fcc1444d6c220c104b7d6e9e5dc
solovyovsdom <joelborofsky@mac.com>
2022-10-31 18:44:46 -0500

    completed lesson 2

---
+++ scroll-triggers-2/main.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.square', {
+  // x: "700",
+  duration: 3,
+  scrollTrigger: {
+    trigger: ".square",
+    start: "top 30%",
+    end: () => `+=${document.querySelector(".square").offsetHeight}`,
+    markers: true,
+    toggleClass: "red"
+    // markers: {
+    //   startColor: "purple",
+    //   endColor: "fuchsia",
+    //   fontSize: "4rem",
+    //   indent: 200
+    // }
+  }
+});
